e38748f55c075af631a3ce586338250aa45be993
ecyrbe <ecyrbe@gmail.com>
2021-03-27 12:22:20 +0100

    feat(command): add init, get , import commands

--- src/apix.ts
+++ src/apix.ts
@@ -1,7 +1,13 @@
 import yargs from 'yargs/yargs';
 import { hideBin } from 'yargs/helpers';
+import * as init from './apix-init';
+import * as get from './apix-get';
+import * as imp from './apix-import';
 
 const argv = yargs(hideBin(process.argv));
 
 argv
+  .command(init.command, init.describe, init.builder, init.handler)
+  .command(get.command, get.describe, get.builder, get.handler)
+  .command(imp.command, imp.describe, imp.builder, imp.handler)
   .help().argv;
